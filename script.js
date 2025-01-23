db2133e8531514c91aa22de473bd43eb2018f8da
anto-design <spartz6122@gmail.com>
2025-01-23 17:06:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  
+  document.addEventListener('DOMContentLoaded', (event)=>{
   const addButton = document.getElementById('add-task-btn')
   const taskInput = document.getElementById('task-input')
   const taskList = document.getElementById('task-list')
@@ -34,4 +34,4 @@ taskList.innerHTML = todoListHTML
 
     renderTodoList();
   };
-  
+})
